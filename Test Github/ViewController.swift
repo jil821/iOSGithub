12e1cc39e86f38cce5c8851b4ef2f0d7e72f18e6
Jeffrey Lim <jeffreylim@Jeffreys-MacBook-Pro-2.local>
2015-01-31 09:25:24 +0800

    Initial Commit

---
+++ Test Github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Github
+//
+//  Created by Jeffrey Lim on 31/1/15.
+//  Copyright (c) 2015 1-Net Singapore Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
